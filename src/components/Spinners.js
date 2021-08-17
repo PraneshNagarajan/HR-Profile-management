08b79738b387b97a53faf51d3249f7682a139993
PraneshNagarajan <sathyajana20@gmail.com>
2021-08-17 13:52:46 +0530

    create component spinners

---
+++ src/components/Spinners.js
@@ -0,0 +1,8 @@
+import './Spinners.css'
+import { Spinner } from "react-bootstrap"
+
+const Spinners = () => {
+    return(<div className="spinner"> <Spinner  animation="border" variant="primary" /></div>)
+}
+
+export default Spinners
