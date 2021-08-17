e060667d2ed32affad113ab906d6546788829a1f
PraneshNagarajan <sathyajana20@gmail.com>
2021-08-17 13:53:26 +0530

    adding spinners component to focalHomepage

--- src/pages/FocalHomePage.js
+++ src/pages/FocalHomePage.js
@@ -5,6 +5,7 @@ import { Col, Row } from "react-bootstrap";
 import Chart from "../components/Charts";
 import { firestore } from "../firebase";
 import { useEffect } from "react";
+import Spinners from "../components/Spinners";
 
 const FocalHomePage = () => {
   const sm = useMediaQuery({ maxWidth: 768 });
@@ -22,82 +23,75 @@ const FocalHomePage = () => {
   }, []);
   return (
     <div>
-      <div
-        className={`d-flex flex-wrap mt-3 ${
-          sm ? `justify-content-center ` : `ms-1`
-        }`}
-      >
-        {datas.map((data) => {
-          return (
-            <Cards
-              color={data.color}
-              width={sm ? "10.5rem" : "11.5rem"}
-              title={sm ? (data.title1 ? data.title1 : data.title) : data.title}
-              data={data.count}
-            />
-          );
-        })}
-      </div>
-      <Row className="d-flex flex-wrap justify-content-between">
-        <Col md="6">
-          {breakups.length > 0 && (
-            <Chart title="Skills Breakup" type="Vbar" datas={breakups[0]} />
-          )}
-        </Col>
-        <Col md={{ span: "6", offset: "" }}>
-          {breakups.length > 0 && (
-            <Chart title="Days Worked" type="Vbar" datas={breakups[0]} />
-          )}
-        </Col>
-        <Col md="6">
-          {breakups.length > 0 && (
-            <Chart title="Overall status" type="line" datas={breakups[4]} />
-          )}
-        </Col>
-        <Col
-          md="6"
-          className={`d-flex${
-            sm ? `-column` : ` flex-wrap justify-content-between`
-          }`}
-        >
-          {breakups.length > 0 && (
-            <Chart
-              title="Submitted breakup"
-              type="Hbar"
-              height={sm ? "" : "45%"}
-              width={sm ? "" : "48%"}
-              datas={breakups[1]}
-            />
-          )}
-          {breakups.length > 0 && (
-            <Chart
-              title="Interview breakup"
-              type="Hbar"
-              height={sm ? "" : "45%"}
-              width={sm ? "" : "48%"}
-              datas={breakups[2]}
-            />
-          )}
-          {breakups.length > 0 && (
-            <Chart
-              title="Offer breakup"
-              type="Hbar"
-              height={sm ? "" : "45%"}
-              width={sm ? "" : "48%"}
-              datas={breakups[3]}
-            />
-          )}
-          {breakups.length > 0 && (
-            <Chart
-              title="Level breakup"
-              type="Hbar"
-              height={sm ? "" : "45%"}
-              width={sm ? "" : "48%"}
-              datas={breakups[1]}
-            />
-          )}
-        </Col>
-      </Row>
+      { !breakups.length > 0 && <Spinners/>}
+      {breakups.length > 0 && (
+        <div>
+          <div
+            className={`d-flex flex-wrap mt-3 ${
+              sm ? `justify-content-center ` : `ms-1`
+            }`}
+          >
+            {datas.map((data) => {
+              return (
+                <Cards
+                  color={data.color}
+                  width={sm ? "10.5rem" : "11.5rem"}
+                  title={
+                    sm ? (data.title1 ? data.title1 : data.title) : data.title
+                  }
+                  data={data.count}
+                />
+              );
+            })}
+          </div>
+          <Row className="d-flex flex-wrap justify-content-between">
+            <Col md="6">
+              <Chart title="Skills Breakup" type="Vbar" datas={breakups[0]} />
+            </Col>
+            <Col md={{ span: "6", offset: "" }}>
+              <Chart title="Days Worked" type="Vbar" datas={breakups[0]} />
+            </Col>
+            <Col md="6">
+              <Chart title="Overall status" type="line" datas={breakups[4]} />
+            </Col>
+            <Col
+              md="6"
+              className={`d-flex${
+                sm ? `-column` : ` flex-wrap justify-content-between`
+              }`}
+            >
+              <Chart
+                title="Submitted breakup"
+                type="Hbar"
+                height={sm ? "" : "45%"}
+                width={sm ? "" : "48%"}
+                datas={breakups[1]}
+              />
+              <Chart
+                title="Interview breakup"
+                type="Hbar"
+                height={sm ? "" : "45%"}
+                width={sm ? "" : "48%"}
+                datas={breakups[2]}
+              />
+              <Chart
+                title="Offer breakup"
+                type="Hbar"
+                height={sm ? "" : "45%"}
+                width={sm ? "" : "48%"}
+                datas={breakups[3]}
+              />
+              <Chart
+                title="Level breakup"
+                type="Hbar"
+                height={sm ? "" : "45%"}
+                width={sm ? "" : "48%"}
+                datas={breakups[1]}
+              />
+            </Col>
+          </Row>
+        </div>
+      )}
     </div>
   );
 };
