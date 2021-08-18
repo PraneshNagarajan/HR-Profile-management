d0d701f39169c537693993a3c57390a6eaeb4879
PraneshNagarajan <sathyajana20@gmail.com>
2021-08-18 20:33:28 +0530

    created date picker to filter datas for charts

---
+++ src/components/DateSearchForm.js
@@ -0,0 +1,54 @@
+import {
+    Col,
+    FormLabel,
+    FormControl,
+    Row,
+    Button,
+  } from "react-bootstrap";
+  import { useMediaQuery } from "react-responsive";
+
+const DateSearchForm = () => {
+    const sm = useMediaQuery({ maxWidth: 768 });
+  return (
+    <Row>
+      <Col
+        md={{ span: "5", offset: "0" }}
+        className="d-flex flex-wrap justify-content-center my-1"
+      >
+        <Col md="2">
+          <FormLabel className="h5 mt-1 d-flex align-self-center mx-1">
+            From
+          </FormLabel>
+        </Col>
+        <Col md="10">
+          <FormControl
+            type="date"
+          />
+        </Col>
+      </Col>
+      <Col
+        md={{ span: "5", offset: "0" }}
+        className="d-flex flex-wrap justify-content-center my-1"
+      >
+        <Col md={{ span: "1", offset: "1" }}>
+          <FormLabel
+            className={`h5 mt-1 d-flex align-self-center ${
+              sm ? `mx-3` : `me-2`
+            }`}
+          >
+            To
+          </FormLabel>
+        </Col>
+        <Col md="10">
+          <FormControl
+            type="date"
+          />
+        </Col>
+      </Col>
+      <Col md="2" className="d-flex justify-content-center my-1">
+        <Button className="w-100" type="submit">Search</Button>
+      </Col>
+    </Row>
+  );
+};
+export default DateSearchForm;
