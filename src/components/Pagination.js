c2c0f39b1c77af13276d6bfd0d648a21b89ab75c
PraneshNagarajan <sathyajana20@gmail.com>
2021-11-11 22:39:47 +0530

    show pagination if total page greater than 0

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,51 +1,82 @@
-import { useDispatch } from "react-redux"
-import { useSelector } from "react-redux"
-import { Pagination} from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { Pagination } from "react-bootstrap";
 import { PaginationActions } from "../Redux/PaginationSlice";
+import { Fragment } from "react/cjs/react.production.min";
 
 const PageSwitcher = () => {
-    const page = useSelector(state => state.pagination);
-    const dispatch = useDispatch();
-    const paginationItems = [];
-    const begin = page.begin;
-    const end = page.end;
-    const current = page.current;
-    const total = page.total
+  const page = useSelector((state) => state.pagination);
+  const dispatch = useDispatch();
+  const paginationItems = [];
+  const begin = page.begin;
+  const end = page.end;
+  const current = page.current;
+  const total = page.total;
 
-    const onCurrentPage = (value) => {
-        dispatch(PaginationActions.current({current : value}))
-    }
-    
-    const onFirst = () => {
-        dispatch(PaginationActions.first())
-    }
-    
-    const onPrevious = () => {
-        dispatch(PaginationActions.previous())
-    }
-    
-    const onNext = () => {
-        dispatch(PaginationActions.next())
-    }
+  const onCurrentPage = (value) => {
+    dispatch(PaginationActions.current({ current: value }));
+  };
 
-    const onLast = () => {
-        dispatch(PaginationActions.last())
-    }
+  const onFirst = () => {
+    dispatch(PaginationActions.first());
+  };
 
-    for(let i = begin; i <= end; i++) {
-        paginationItems.push(
-            <Pagination.Item key={i} className={i === current ? 'active'  : ""} onClick={() => onCurrentPage(i)}>{i}</Pagination.Item>
-        )
-    }
-    
-    return(
+  const onPrevious = () => {
+    dispatch(PaginationActions.previous());
+  };
+
+  const onNext = () => {
+    dispatch(PaginationActions.next());
+  };
+
+  const onLast = () => {
+    dispatch(PaginationActions.last());
+  };
+
+  for (let i = begin; i <= end; i++) {
+    paginationItems.push(
+      <Pagination.Item
+        key={i}
+        className={i === current ? "active" : ""}
+        onClick={() => onCurrentPage(i)}
+      >
+        {i}
+      </Pagination.Item>
+    );
+  }
+
+  return (
+    <Fragment>
+      {total > 0 && (
         <Pagination>
-        <Pagination.Item disabled={current === 1 ? true : false} onClick={() => onFirst(1)} >{"<<"}</Pagination.Item>
-        <Pagination.Item disabled={current === 1 ? true : false} onClick={() => onPrevious(current - 1)} >{"<"}</Pagination.Item>
-        {paginationItems}
-        <Pagination.Item disabled={total === end && end === current ? true : false} onClick={() => onNext(current + 1)} >{">"}</Pagination.Item>
-        <Pagination.Item disabled={total === end && end === current ? true : false} onClick={() => onLast(total)} >{">>"}</Pagination.Item>
-      </Pagination>
-    )
-}
-export default PageSwitcher
+          <Pagination.Item
+            disabled={current === 1 ? true : false}
+            onClick={() => onFirst(1)}
+          >
+            {"<<"}
+          </Pagination.Item>
+          <Pagination.Item
+            disabled={current === 1 ? true : false}
+            onClick={() => onPrevious(current - 1)}
+          >
+            {"<"}
+          </Pagination.Item>
+          {paginationItems}
+          <Pagination.Item
+            disabled={total === end && end === current ? true : false}
+            onClick={() => onNext(current + 1)}
+          >
+            {">"}
+          </Pagination.Item>
+          <Pagination.Item
+            disabled={total === end && end === current ? true : false}
+            onClick={() => onLast(total)}
+          >
+            {">>"}
+          </Pagination.Item>
+        </Pagination>
+      )}
+    </Fragment>
+  );
+};
+export default PageSwitcher;
