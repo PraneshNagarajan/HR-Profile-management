6b2bd51895dcc0a64e7b9eb661bc96c696e20814
PraneshNagarajan <sathyajana20@gmail.com>
2021-11-11 20:19:00 +0530

    created filterProfileSlice

---
+++ src/Redux/FilterProfileSlice.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const onCatagoryFilterHandler = (dataLists, options) => {
+    let datas = options.length > 0 ? [] : dataLists;
+    dataLists.map((item, index) => {
+      if (options.includes(item.status)) {
+        datas.push(item);
+      }
+    });
+    return datas;
+}
+
+const initialState = {
+    result: [],
+    errors: "",
+    flag: false
+}
+
+const FilterProfileSlice =  createSlice({
+name: "FilterProfiles",
+initialState,
+reducers: {
+    onSetInitial(state){
+        state.result = []
+        state.errors = ""
+        state.flag = false
+    },
+    onTextFilterHandler(state, action) {
+        state.flag = true
+        let result = [];
+        action.payload.data.map((item, index) => {
+          if (item.id.includes(action.payload.id)) {
+            result.push(item);
+          }
+          if (action.payload.data.length - 1 === index && result.length > 0) {
+            let output = onCatagoryFilterHandler(
+                action.payload.id.length > 0 || action.payload.length > 0 ? result : action.payload.data,
+              action.payload.options
+            );
+            result = output;
+            state.result = output
+          }
+        });
+        state.errors = result.length === 0 ? "No match found." : ""
+    },
+}
+})
+
+export const FilterProfileActions = FilterProfileSlice.actions;
+export default FilterProfileSlice.reducer;
